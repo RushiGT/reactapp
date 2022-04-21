6d0308d06febce1c96e6820f9a5b83ad21c6dc97
RushiGT <rushipateltek@gmail.com>
2022-04-21 14:01:42 +0530

    LoadingComponentAdded

--- src/app/components/restaurantpage/components/restaurantbody/Restaurantbody.js
+++ src/app/components/restaurantpage/components/restaurantbody/Restaurantbody.js
@@ -3,20 +3,21 @@ import RestaurantHeader from './components/restaurantHeader';
 import RestaurantDetails from './components/restaurantDetails';
 import RestaurantMenu from './components/restaurantMenu';
 import RestaurantFooter from './components/restaurantFooter';
+import Loading from './components/loading';
 import { getRestaurantObjectDetails } from '../../../../../redux/reducers/selectors/getRestaurantDetails';
 import { connect } from 'react-redux';
 import {  fetchRestaurant } from '../../../../../redux/actions/RestaurantActions';
 function RestaurantBody(props) {
    let {fetchRestaurantHere,restaurantObject} = props;
     useEffect(()=>{
-            fetchRestaurantHere();
+           setTimeout(()=>{fetchRestaurantHere()},1000); //fetchRestaurantHere();
     },[]);
 
    
     return (
         
         <div className="restaurantcontent">
-            {(restaurantObject.loading || Object.keys(restaurantObject.data).length === 0) ? <div className = "loading">Loading</div> : <> <RestaurantHeader />
+            {(restaurantObject.loading || Object.keys(restaurantObject.data).length === 0) ? <Loading /> : <> <RestaurantHeader />
             <RestaurantDetails />
             <RestaurantMenu />
             <RestaurantFooter /></>}
---
+++ src/app/components/restaurantpage/components/restaurantbody/components/loading/Loading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Loading() {
+  return (
+    <div className='Loading'>Loading, please wait...</div>
+  )
+}
+
+export default Loading;
